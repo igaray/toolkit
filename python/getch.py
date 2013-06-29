f272b3494cafe1d4c077c2389591a81518e75922
Iñaki Garay <igarai@gmail.com>
2013-06-28 23:54:35 -0300

    Added getch.py

---
+++ python/getch.py
@@ -0,0 +1,13 @@
+# Get a character in raw mode from stdin, without having to press enter.
+
+def getch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+print("you pressed: ", getch())
