cf6f4be2cf217e0ef620682f6c1f6524fa7200e8
Iñaki Garay <igarai@gmail.com>
2016-05-07 09:46:58 -0300

    YAML better error handling

--- c/yaml/src/main.c
+++ c/yaml/src/main.c
@@ -1,6 +1,16 @@
 #include "yaml.h"
 #include <stdio.h>
 
+typedef enum return_values {
+  OK,
+  ERR_ARG_NUM,
+  ERR_FILE_OPEN_FAIL,
+  ERR_YAML_INIT_FAIL,
+  ERR_DOC_LOAD_FAIL,
+  ERR_PARSE_FAIL,
+  ERR_UNEXPECTED_NODE_TYPE
+} return_value_t;
+
 void yaml_version() {
   int major = 0, minor = 0, patch = 0;
   yaml_get_version(&major, &minor, &patch);
@@ -56,35 +66,36 @@ int print_document(yaml_document_t *document, yaml_node_t *node) {
   }
   default: {
     printf("ERROR: unexpected node type: %d\n", node->type);
-    rv = -5;
+    rv = ERR_UNEXPECTED_NODE_TYPE;
   }
   }
   return rv;
 }
 
 int load_documents(char *config_file_path) {
+  int rv = 0;
   FILE *config_file = NULL;
   yaml_parser_t parser;
   yaml_document_t document;
-  int done = 0;
 
   config_file = fopen(config_file_path, "r");
 
   if (config_file == NULL) {
     printf("ERROR: failed to open file %s\n", config_file_path);
-    return -1;
+    return ERR_FILE_OPEN_FAIL;
   }
   if (!yaml_parser_initialize(&parser)) {
     printf("ERROR: failed to initialize YAML parser\n");
-    return -2;
+    return ERR_YAML_INIT_FAIL;
   }
 
   yaml_parser_set_input_file(&parser, config_file);
 
+  int done = 0;
   while (!done) {
     if (!yaml_parser_load(&parser, &document)) {
       printf("ERROR: failed to load document\n");
-      return -3;
+      return ERR_DOC_LOAD_FAIL;
     }
 
     yaml_node_t *root_node = yaml_document_get_root_node(&document);
@@ -97,23 +108,22 @@ int load_documents(char *config_file_path) {
   }
   yaml_parser_delete(&parser);
   fclose(config_file);
-  return 0;
+  return rv;
 }
 
 int main(int argc, char *argv[]) {
-  int error = 0;
+  int rv = OK;
 
   if (argc != 2) {
     printf("ERROR: incorrect number of arguments\n");
-    return -1;
+    return ERR_ARG_NUM;
   }
 
-  yaml_version();
+  // yaml_version();
 
-  error = load_documents(argv[1]);
-  if (error) {
-    printf("ERROR: %d", error);
-    return error;
+  rv = load_documents(argv[1]);
+  if (rv) {
+    printf("ERROR: %d", rv);
   }
-  return 0;
+  return rv;
 }
