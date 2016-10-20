fa7e51c5a84b22287fed446cca30ce98b6b8dfd5
Iñaki Garay <igarai@gmail.com>
2016-10-20 12:54:17 -0300

    Treeshadow script and file attrs

--- python/treeshadow.py
+++ python/treeshadow.py
@@ -1,16 +1,21 @@
 import os
+import sys
 
 def main():
-  # os.mkdir("./shadow/")
-  # Obtain all program source files.
-  for path, subdirs, files in os.walk("."):
-    if os.stat(path).is_dir(path):
-      # os.mkdir("./shadow" + path)
-      print(path)
-      pass
-    else:
-      pass
-      # f = open(path + , "w")
+  if len(sys.argv) == 3:
+    PREFIX = sys.argv[2]
+  else:
+    PREFIX = "./treeshadow"
+  os.makedirs(PREFIX, exist_ok=True)
+
+  for path, subdirs, files in os.walk(sys.argv[1]):
+    for directoryname in subdirs:
+      print("D", PREFIX + path + "/" + directoryname)
+      os.makedirs(PREFIX + path + "/" + directoryname, exist_ok=True)
+    for filename in files:
+      print("    F", PREFIX + path + "/" + filename)
+      f = open(PREFIX + path + "/" + filename, "w")
+      f.close()
 
 if __name__ == "__main__":
   main()
