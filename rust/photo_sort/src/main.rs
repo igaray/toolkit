b1a69cdd8f3c5a7a116b2d48eb8ccfd884832020
Iñaki Garay <igarai@gmail.com>
2017-11-09 17:52:13 -0300

    Suggestions

--- rust/photo_sort/src/main.rs
+++ rust/photo_sort/src/main.rs
@@ -9,13 +9,15 @@ extern crate clap;
 #[macro_use]
 extern crate lazy_static;
 extern crate regex;
+extern crate walkdir;
 
 use clap::{Arg, App};
 use regex::Regex;
 use std::fs;
 use std::io;
 use std::option;
-use std::path;
+use std::path::{Path,PathBuf};
+use walkdir::WalkDir;
 
 fn main() {
     let path_arg = Arg::with_name("path")
@@ -59,18 +61,17 @@ fn main() {
         .arg(undo_arg)
         .get_matches();
 
-    let workdir: &path::Path;
+    let workdir: &Path;
     if args.is_present("path") {
-        workdir = path::Path::new(args.value_of("path").unwrap());
+        workdir = Path::new(args.value_of("path").unwrap());
     }
     else {
-        workdir = path::Path::new("./");
+        workdir = Path::new("./");
     }
 
-    let cb = | x: &std::fs::DirEntry | {
-        process_entry(&args, &workdir, &x.path());
-    };
-    visit_dirs(workdir, &cb).unwrap();
+    for entry in WalkDir::new(workdir) {
+        process_entry(&args, &workdir, &entry.unwrap().path())
+    }
 
     if !args.is_present("dry-run") && args.is_present("undo") {
         if !args.is_present("silent") {
@@ -79,90 +80,58 @@ fn main() {
     }
 }
 
-fn process_entry(args: &clap::ArgMatches, workdir: &path::Path, file_path: &path::Path) {
-    match destination(&workdir, &file_path) {
-        Some(destination) => {
-            match create_directory(&args, &destination) {
-                Ok(_) => {
-                    match move_file(&args, &file_path, &destination) {
-                        Ok(_) => {}
-                        Err(reason) => {
-                            if !args.is_present("silent") {
-                                println!("Error moving file {:?}, reason: {:?}", file_path, reason);
+fn process_entry(args: &clap::ArgMatches, workdir: &Path, entry: &Path) {
+    if !entry.is_dir() {
+        match destination(&workdir, &entry) {
+            Some(destination) => {
+                match create_directory(&args, &destination) {
+                    Ok(_) => {
+                        match move_file(&args, &entry, &destination) {
+                            Ok(_) => {}
+                            Err(reason) => {
+                                if !args.is_present("silent") {
+                                    println!("Error moving file {:?}, reason: {:?}", entry, reason);
+                                }
                             }
                         }
-                    }
-                },
-                Err(reason) => {
-                    if !args.is_present("silent") {
-                        println!("Unable to create directory {:?}, reason: {:?}", destination, reason);
+                    },
+                    Err(reason) => {
+                        if !args.is_present("silent") {
+                            println!("Unable to create directory {:?}, reason: {:?}", destination, reason);
+                        }
                     }
                 }
-            }
-        },
-        None => {}
-    }
-}
-
-fn visit_dirs(dir: &path::Path, cb: &Fn(&std::fs::DirEntry)) -> io::Result<()> {
-    if dir.is_dir() {
-        for entry in fs::read_dir(dir)? {
-            let entry = entry?;
-            let path = entry.path();
-            if path.is_dir() {
-                visit_dirs(&path, cb)?;
-            } else {
-                cb(&entry);
-            }
+            },
+            None => {}
         }
     }
-    Ok(())
 }
 
-fn destination(base_dir: &path::Path, file_name: &path::Path) -> option::Option<path::PathBuf> {
-    match file_name.to_str() {
-        Some(filename) => {
-            match date(filename) {
-                Some((year, month, day)) => {
-                    let dir = format!("{}-{}-{}", year, month, day);
-                    let mut dest = path::PathBuf::new();
-                    dest.push(base_dir);
-                    dest.push(dir);
-                    Some(dest)
-                },
-                None => None
-            }
-        },
-        None => None
-    }
+fn destination(base_dir: &Path, file_name: &Path) -> option::Option<PathBuf> {
+    file_name.to_str().and_then(date).map(|(year, month, day)| {
+        base_dir.join(format!("{}-{}-{}", year, month, day))
+    })
 }
 
 fn date(filename: &str) -> Option<(&str, &str, &str)> {
 
     lazy_static! {
-        static ref RE: Regex = Regex::new(r"/D*(20[01][0-9]).?([01][0-9]).?([0123][0-9])\D*").unwrap();
+        // YYYY?MM?DD or YYYYMMDD,
+        // where YYYY in [2000-2019], MM in [01-12], DD in [01-31]
+        static ref RE: Regex = Regex::new(r"\D*(20[01]\d).?(0[1-9]|1[012]).?(0[1-9]|[12]\d|30|31)\D*").unwrap();
     }
 
-    if RE.is_match(filename) {
-        match RE.captures(filename) {
-            Some(captures) => {
-                let year = captures.get(1).unwrap().as_str();
-                let month = captures.get(2).unwrap().as_str();
-                let day = captures.get(3).unwrap().as_str();
-                let tuple = (year, month, day);
-                Some(tuple)
-            },
-            None => None
-        }
-    }
-    else {
-        None
-    }
+    RE.captures(filename).map(|captures| {
+        let year = captures.get(1).unwrap().as_str();
+        let month = captures.get(2).unwrap().as_str();
+        let day = captures.get(3).unwrap().as_str();
+        (year, month, day)
+    })
 }
 
-fn create_directory(args: &clap::ArgMatches, directory: &path::Path) -> io::Result<()> {
+fn create_directory(args: &clap::ArgMatches, directory: &Path) -> io::Result<()> {
     if !args.is_present("dry-run") {
-        let mut full_path = path::PathBuf::new();
+        let mut full_path = PathBuf::new();
         full_path.push(directory);
         match fs::create_dir(full_path) {
             Ok(_) => {
@@ -188,8 +157,8 @@ fn create_directory(args: &clap::ArgMatches, directory: &path::Path) -> io::Resu
     }
 }
 
-fn move_file(args: &clap::ArgMatches, from: &path::Path, dest: &path::Path) -> io::Result<()> {
-    let mut to = path::PathBuf::new();
+fn move_file(args: &clap::ArgMatches, from: &Path, dest: &Path) -> io::Result<()> {
+    let mut to = PathBuf::new();
     to.push(dest);
     to.push(from.file_name().unwrap());
 
