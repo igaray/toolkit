ffad77255ec12273db05eaa12b93fd2a8f3b3746
Iñaki Garay <igarai@gmail.com>
2018-02-14 16:21:21 -0300

    Change actions storage to HashMap

--- rust/mmass/src/mmass/action.rs
+++ rust/mmass/src/mmass/action.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 // Actions
 pub enum ActionKind {
   Noop,
@@ -11,34 +13,31 @@ pub struct Action {
 }
 
 impl Action {
-  pub fn new() -> Action {
-    let action = Action{id: 0, kind: ActionKind::Noop};
+  pub fn new(id: u64, kind: ActionKind) -> Action {
+    let action = Action{id: id, kind: kind};
     return action
   }
 }
 
 pub struct Actions {
-  _data: Vec<Action>
+  _data: HashMap<u64, Action>
 }
 
 impl Actions {
   pub fn new() -> Actions {
-    return Actions{_data: Vec::new()}
+    return Actions{_data: HashMap::new()}
   }
 
-  pub fn add(&self, action: Action) -> Result<(), ()> {
-    let res = Ok(());
-    return res
+  pub fn add(&mut self, action: Action) {
+    self._data.insert(action.id, action);
   }
 
-  pub fn remove(&self, id: u64) -> Result<(), ()> {
-    let res = Ok(());
-    return res
+  pub fn remove(&mut self, id: u64) {
+    self._data.remove(&id);
   }
 
-  pub fn find(&self, id: u64) -> Result<Action, ()> {
-    let action = Action{id: 0, kind: ActionKind::Noop};
-    let res = Ok(action);
+  pub fn find(&self, id: u64) -> Option<&Action> {
+    let res = self._data.get(&id);
     return res
   }
 }
