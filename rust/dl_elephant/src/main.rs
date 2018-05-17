7c99f7b49d760475d5f273c577dc9ebaf7064a51
Iñaki Garay <igarai@gmail.com>
2018-05-17 00:49:42 -0300

    dl_elephant

--- rust/dl_elephant/src/main.rs
+++ rust/dl_elephant/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+extern crate notify;
+
+use notify::{RecommendedWatcher, Watcher, RecursiveMode};
+use std::sync::mpsc::channel;
+use std::time::Duration;
+
+fn watch() -> notify::Result<()> {
+  let (tx, rx) = channel();
+  let mut watcher: RecommendedWatcher = try!(Watcher::new(tx, Duration::from_secs(2)));
+  try!(watcher.watch("/Users/aki/Downloads/", RecursiveMode::Recursive));
+  loop {
+    match rx.recv() {
+      Ok(event) => println!("{:?}", event),
+      Err(e) => println!("watch error: {:?}", e),
+    }
+  }
 }
+  
+fn main() {
+  if let Err(e) = watch() {
+    println!("error: {:?}", e)
+  }
+}
+
