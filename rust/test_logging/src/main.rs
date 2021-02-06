bb1b9942199231e0154a9057b600c5c9a1e44e3d
Iñaki Garay <igarai@gmail.com>
2021-02-06 12:05:49 -0300

    rust playground

--- rust/test_logging/src/main.rs
+++ rust/test_logging/src/main.rs
@@ -3,10 +3,34 @@ extern crate log;
 extern crate env_logger;
 
 use std::{thread, time};
+pub mod sort {
+    pub fn insertion(a: &mut [usize; 10]) {
+        let n = 10;
+        let mut i = 1;
+        while i < n {
+          let mut j = i;
+          while j > 0 && a[j-1] > a[j] {
+            let t = a[j-1];
+            a[j-1] = a[j];
+            a[j] = t;
+            j -= 1;
+            debug!("a: {:?}", a);
+          }
+          i += 1;
+        }
+      }
+}
 
+  fn test() {
+    let mut a = [2,6,4,7,5,8,3,9,1,0];
+    sort::insertion(&mut a);
+    debug!("a: {:?}", a);
+  }
+  
 fn main() {
     let one_second = time::Duration::from_millis(1000);
     env_logger::init();
+    test();
     loop {
         trace!(target: "test", "trace");
         debug!("debug");
